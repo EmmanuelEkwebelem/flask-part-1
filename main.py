e3733fb06f3c3aed8b7a72235e945a1f9549f309
EmmanuelEkwebelem <93545665+EmmanuelEkwebelem@users.noreply.github.com>
2022-12-15 12:56:40 -0500

    Update

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('home.html')
+    return render_template('homepage.html')
 
-@app.route('/login/')
+@app.route('/about/')
 def about():
-    return render_template('login.html')
+    return render_template('about.html')
 
-@app.route('/signup/')
+@app.route('/contact/')
 def contact():
-    return render_template('signup.html')
+    return render_template('contact.html')
 
 if  __name__=='__main__':
     app.run(host="0.0.0.0", port=8080, debug=True)
